7b27baf32b00dc21d325ec9280fc17665d407ab9
damian-sketch <cedricdamian19@gmail.com>
2023-05-25 18:35:25 +0300

    Implement endpoint to register users

--- app.py
+++ app.py
@@ -1,10 +1,60 @@
-from flask import Flask
+from flask import Flask, request
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import yaml
+import re
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '<h1>Hello from Flask!</h1>'
+# Parse yaml file
+with open('db.yaml', 'r') as file:
+    db = yaml.safe_load(file)
+
+# Configure db
+app.config['MYSQL_HOST'] = db['mysql_host']
+app.config['MYSQL_USER'] = db['mysql_user']
+app.config['MYSQL_PASSWORD'] = db['mysql_password']
+app.config['MYSQL_DB'] = db['mysql_db']
+
+mysql = MySQL(app)
+
+# register new account
+
+
+@app.route('/', methods=['GET', 'POST'])
+def register():
+    msg = ''
+    if (request.method == 'POST' and 'username' in request.form
+        and 'password' in request.form and 'email' in request.form
+            and 'address' in request.form and 'phone_number' in request.form):
+        # Store all the values from the request
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        address = request.form['address']
+        phone_number = request.form['phone_number']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute(
+            'SELECT * FROM users WHERE email = % s', (email, ))
+        account = cursor.fetchone()
+
+        # Validations for the account details
+        if account:
+            msg = 'Account already exists !'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address !'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'name must contain only characters and numbers !'
+        else:
+            cursor.execute("INSERT INTO users(username,password,email,phone_number,address) VALUES(%s, %s, %s, %s, %s)",
+                           [username, password, email, phone_number, address])
+            mysql.connection.commit()
+            msg = 'You have successfully registered !'
+    elif request.method == 'POST':
+        msg = 'Please fill out the form !'
+    return msg
+
 
 if __name__ == '__main__':
     app.run(debug=True)
